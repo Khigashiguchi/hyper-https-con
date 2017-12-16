38647822a0139fbd08e7ed900a7541f8f183c2d8
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-16 15:44:20 +0900

    Implement parse json response

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,34 @@
 extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
+extern crate serde_json;
 
-//use std::io::{self, Write};
+use std::io;
 use futures::{Future, Stream};
 use hyper::Client;
 use tokio_core::reactor::Core;
-use hyper::{Method, Request};
-use hyper::header::{ContentLength, ContentType};
+use serde_json::Value;
 
 fn main() {
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
 
-    let json = r#"{"library":"hyper"}"#;
-    let uri = "http://httpbin.org/post".parse().unwrap();
-    let mut req = Request::new(Method::Post, uri);
-    req.headers_mut().set(ContentType::json());
-    req.headers_mut().set(ContentLength(json.len() as u64));
-    req.set_body(json);
-    let post = client.request(req).and_then(|res| {
-        println!("POST: {}", res.status());
-
-        res.body().concat2()
+    let uri = "http://httpbin.org/ip".parse().unwrap();
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+        res.body().concat2().and_then(move |body| {
+            let v: Value = serde_json::from_slice(&body).unwrap();
+            /*TODO: エラーハンドリング
+            .map_err(|e| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    e
+                );
+            });
+            */
+            println!("Current IP address is {}", v["origin"]);
+            Ok(())
+        })
     });
-    // Multiple request
-    //let get_uri = "http://httpbin.org/headers".parse().unwrap();
-    //let get = client.get(get_uri).and_then(|res| {
-    //    println!("GET: {}", res.status());
-
-    //    res.body().concat2();
-    //});
-    //let work = post.join(get);
-    core.run(post).unwrap();
+    core.run(work);
 }
-

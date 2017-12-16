1b83c70fd074f4d79f4a4851de7bbde883076b03
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-16 16:01:43 +0900

    Implement https connection

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Reference
 - [hyper.rs](https://hyper.rs/guides/)
 - [example code: client.rs](https://github.com/hyperium/hyper/blob/master/examples/client.rs)
+- [unwrap()]()
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
-extern crate futures;
 extern crate hyper;
+extern crate hyper_tls;
 extern crate tokio_core;
-extern crate serde_json;
 
-use std::io;
-use futures::{Future, Stream};
 use hyper::Client;
+use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
-use serde_json::Value;
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let handle = core.handle();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
 
-    let uri = "http://httpbin.org/ip".parse().unwrap();
-    let work = client.get(uri).and_then(|res| {
-        println!("Response: {}", res.status());
-        res.body().concat2().and_then(move |body| {
-            let v: Value = serde_json::from_slice(&body).unwrap();
-            /*TODO: エラーハンドリング
-            .map_err(|e| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    e
-                );
-            });
-            */
-            println!("Current IP address is {}", v["origin"]);
-            Ok(())
-        })
-    });
-    core.run(work);
+    let req = client.get("https://hyper.rs".parse().unwrap());
+    let res = core.run(req).unwrap();
+    assert!(res.status().is_success());
 }
